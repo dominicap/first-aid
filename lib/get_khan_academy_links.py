ebc250e4db321744ed9214c1a9173a95e8f797ce
Dominic Philip <domi.a.philip@gmail.com>
2017-03-25 22:47:14 -0400

    refactor to get unique links

--- lib/get_khan_academy_links.py
+++ lib/get_khan_academy_links.py
@@ -8,22 +8,29 @@ YOUTUBE_CREDENTIALS = str(youtube_key_file.read().strip())
 KHAN_ACADEMY_CHANNEL_ID = 'UC4a-Gbdw7vOaccHmFo40b9g'
 PAGE_TOKEN = ''
 
-TOTAL_RESULTS = 10
+TOTAL_RESULTS = 50
 
 result_level = 1
 while (result_level <= math.ceil(float(TOTAL_RESULTS) / 50.0)):
-    print("At level " + str(result_level))
 
-    URL = "https://www.googleapis.com/youtube/v3/search?key=" + YOUTUBE_CREDENTIALS + "&channelId=" + KHAN_ACADEMY_CHANNEL_ID + "&part=snippet,id&order=date&maxResults=50&pageToken" + PAGE_TOKEN
+    URL = "https://www.googleapis.com/youtube/v3/search?key=" + YOUTUBE_CREDENTIALS + "&channelId=" + KHAN_ACADEMY_CHANNEL_ID + "&part=snippet,id&order=date&maxResults=50&pageToken=" + PAGE_TOKEN
 
     results = json.loads(urllib.request.urlopen(URL).read().decode('utf-8'))
 
-    if (result_level == 1):
+    try:
         PAGE_TOKEN = results['nextPageToken']
+    except KeyError:
+        print('REQUEST LIMIT')
+        quit()
+
+    if (result_level == 1):
         TOTAL_RESULTS = int(results['pageInfo']['totalResults'])
 
     for result in results['items']:
         with open('../data/video_ids.txt', 'a+') as file:
-            file.write(result['id']['videoId'] + '\n')
+            try:
+                file.write("http://youtube.com/watch?v=" + result['id']['videoId'] + '\n')
+            except KeyError:
+                pass
 
     result_level = result_level + 1
